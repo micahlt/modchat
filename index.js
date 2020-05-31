cf4f181277157bef249e3d51d7ee8c9a6edbeae0
micahlt <micahincorporated@gmail.com>
2020-05-30 21:22:50 -0500

    A quick-and-dirty ScratchVerifier auth solution

--- index.js
+++ index.js
@@ -1,25 +1,69 @@
-var express = require('express');
-var app = express();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-var Filter = require('bad-words');
-var filter = new Filter();
-app.use(express.static(__dirname + '/public'));
+// Import all needed modules
+global.fetch = require("node-fetch"); // for web requests
+global.btoa = require('btoa'); // for SV authenication
+var express = require('express'); // for main server
+var Filter = require('bad-words'); // for filtering messages
+var app = express(); // define the app var
+var http = require('http').createServer(app); // init http server
+var io = require('socket.io')(http); // attach socket to the server
+var filter = new Filter(); // set up the filter
+var svAppId = "4205845"; // register SV app id
+var svAppSecret = "58402c158faf27abf7e89e723672d315c9a7bf40be0e7cb6bae2d8dcde886a0b"; // register SV app secret (token)
+app.use(express.static(__dirname + '/public')); // tell express where to get public assets
 
-app.get('/', (req, res) => {
+app.get('/', (req, res) => { // set root location to index.html
   res.sendFile(__dirname + '/index.html');
 });
-io.on('connection', (socket) => {
-  console.log('a user connected' /* + user */ );
-  socket.on('chatMessage', (msg) => {
-    console.log('message: ' + filter.clean(msg));
-
-    io.emit('chatMessage', filter.clean(msg));
+io.on('connection', (socket) => { // handle a user connecting
+  console.log('a user connected' /* + user */ ); // ROP
+  socket.on('chatMessage', (msg) => { // handle the server recieving messages
+    io.emit('chatMessage', filter.clean(msg)); // clean and then send the message to all clients
   });
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  socket.on('userRegister', (msg) => { // handle user registration
+    fetch('https://api.scratch.mit.edu/users/' + msg) // make a request to the Scratch API
+      .then(response => response.json())
+      .then(data => {
+        if (data.id == null) { // make sure that the user exists
+          console.log("user doesn't exist"); // ROP
+        } else { // if they do exist, continue with registration
+          console.log("confirming user id " + data.id); // ROP
+          fetch('http://scratchverifier.ddns.net:8888/verify/' + msg, { // make a request to the SV server
+            method: 'PUT',
+            headers: {
+              'Authorization': "Basic " + btoa(svAppId + ":" + svAppSecret) // use basic token auth to connect
+            }
+          }).then((response) => {
+            return response.json();
+          }).then((data) => {
+            console.log(data.code); // ROP
+            socket.emit("svCodeToVerify", data.code); // send the SV verification code back to the registering user
+            socket.on('finishVerification', (msgTwo) => { // handle finishing verification
+              console.log("AHH VERIFYY"); // ROP
+              fetch('http://scratchverifier.ddns.net:8888/verify/' + msg, { // make a request to the SV server (again)
+                method: 'POST',
+                headers: {
+                  'Authorization': "Basic " + btoa(svAppId + ":" + svAppSecret) // use basic token auth again
+                }
+              }).then((response) => {
+                return response.ok;
+              }).then((data) => {
+                console.log('Response: ' + data); // ROP
+                if (data) { // if the response was okay
+                  socket.emit("verificationSuccess", msg); // Send a success message to the registering user
+                } else { // if verification failed
+                  // generate error here...
+                  console.error("Error with verification: " + data); // ROP
+                }
+              })
+            })
+          })
+        }
+      });
+  });
+  socket.on('disconnect', () => { // handle user disconnecting from the server
+    console.log('user disconnected'); // ROP
   });
 });
-http.listen((process.env.PORT || 80), () => {
-  console.log('listening on port 3000');
+http.listen((process.env.PORT || 3001), () => { // initialize the server
+  console.log('listening on a port'); // ROP
 });
--- public/app.js
+++ public/app.js
@@ -1,17 +1,45 @@
-document.getElementById("form").addEventListener("submit", function(event) {
-  event.stopImmediatePropagation();
-  event.preventDefault();
-  socket.emit('chatMessage', document.getElementById("m").value);
-  document.getElementById("m").value = "";
+var socket = io(); // define socket
+var userObject = window.localStorage.getItem("userObject"); // grab the user object from localStorage if it exists
+if (userObject) { // if the user object contains a name
+  console.log("User has already verified"); // ROP
+  document.getElementsByClassName("blocker")[0].style.display = "none";
+  document.getElementsByClassName("register")[0].style.display = "none";
+}
+document.getElementById("form").addEventListener("submit", function(event) { // listen for submits on the message sending form
+  event.stopImmediatePropagation(); // stop reloads
+  event.preventDefault(); // stop reloads
+  socket.emit('chatMessage', document.getElementById("m").value); // send the chat message from the form value to the server
+  document.getElementById("m").value = ""; // reset the chat form's value
   return false;
-})
-var socket = io();
-socket.on('chatMessage', function(msg) {
-  var m = document.createElement('li');
-  m.innerText = msg;
-  document.getElementById('messages').appendChild(m);
+});
+document.getElementById("username").addEventListener("submit", function(event) { // listen for user registration
+  event.stopImmediatePropagation(); // stop reloads
+  event.preventDefault(); // stop reloads
+  socket.emit('userRegister', document.getElementById("username-input").value); // send the username to verify to the server
+  return false;
+});
+socket.on('chatMessage', function(msg) { // handle recieving chat messages
+  var m = document.createElement('li'); // create an element to display the message
+  m.innerText = msg; // add the message text to that element
+  document.getElementById('messages').appendChild(m); // append the message to the message area
 });
 
+socket.on('svCodeToVerify', function(msg) { // handle recieving the SV code (after triggering the setUsername function)
+  document.getElementById('svCode').innerText = msg; // display the code
+  document.getElementById('completeSV').style.display = "block"; // display the completion button
+  document.getElementById('completeSV').addEventListener('click', function() { // listen for clicks on the completion button
+    socket.emit('finishVerification'); // tell the server to finish verification
+  })
+});
+
+socket.on('verificationSuccess', function(msg) { // handle a successful verification with SV
+  window.localStorage.setItem("userObject", {
+    "name": msg,
+    "verified": true
+  });
+  window.location.reload();
+})
+
 function setUsername() {
-  socket.emit('setUsername', document.getElementById('name').value);
+  socket.emit('setUsername', document.getElementById('name').value); // tell the server to begin SV registration
 };
